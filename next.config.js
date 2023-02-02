0b857787c2f174d219348de41b8a559e053aa405
naveen nautiyal <naveennautiyal10@yahoo.com>
2023-02-02 16:17:35 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   images: {
-//     domains: ["links.papareact.com", "image.tmdb.org"],
-//   },
-// };
-
-// module.exports = nextConfig;
-
 module.exports = {
   images: {
     domains: ["links.papareact.com", "image.tmdb.org"],
